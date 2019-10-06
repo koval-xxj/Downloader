499d5f0b68592f73cdce5a1949391beb374983e0
Сергей Коваленко <koval.xxj@gmail.com>
2019-10-06 21:13:29 +0300

    Правки

--- classes/Downloader.php
+++ classes/Downloader.php
@@ -2,10 +2,27 @@
 
 class Downloader
 {
-    // Attempts to create a stream
+    /**
+     * Attempts to create a stream
+     */
     const CONN_ATTEMTS_NUM = 3;
+    const DEBUG = true;
 
+    /**
+     * Perpetual cycle loop insurance
+     */
+    const WAITING_FOR_DATA_ATTEMTS = 50;
+
+    /**
+     * Download files path
+     * @var string
+     */
     private $downloadPath;
+
+    /**
+     * Log files path
+     * @var string
+     */
     private $logPath;
 
     private $ip;
@@ -14,19 +31,26 @@ class Downloader
     private $path;
 
     private $streamsNum;
-
     private $fileInfo = [];
-    private $bar = null;
 
+    /**
+     * Initialization
+     *
+     * @param string $url - file url
+     * @param int - $streamsNum - streams number
+     *
+     * @return void
+     */
     public function __construct($url, $streamsNum = 4)
     {
-        ini_set('memory_limit', '128M'); // для теста, удалить
-
-        if ( empty($url) || !$parsed_url = parse_url($url) )
+        if ( empty($url) || !$url = trim($url) )
             throw new RuntimeException("The param url must not be empty");
 
         if ( !defined('PATH_ROOT') )
             throw new RuntimeException("The constant 'PATH_ROOT' is not defined");
+
+        $streamsNum = intval($streamsNum);
+        $this->fileInfo = $this->GetUrlInfo($url);
 
         $this->downloadPath = PATH_ROOT.'downloads/';
         $this->logPath = PATH_ROOT.'log/';
@@ -34,25 +58,34 @@ class Downloader
         if ( !file_exists($this->downloadPath) && !mkdir($this->downloadPath) )
             throw new RuntimeException("Unable to create the directory {$this->downloadPath}");
 
-        if ( !file_exists($this->logPath) && !mkdir($this->logPath) )
+        if ( self::DEBUG && !file_exists($this->logPath) && !mkdir($this->logPath) )
             throw new RuntimeException("Unable to create the directory {$this->logPath}");
 
-        $this->ip = gethostbyname($parsed_url['host']);
-        $this->host = $parsed_url['host'];
-        $this->port = getservbyname($parsed_url['scheme'], 'tcp');
-        $this->path = $parsed_url['path'];
+        $this->ip = gethostbyname($this->fileInfo['host']);
+        $this->host = $this->fileInfo['host'];
+        $this->port = getservbyname($this->fileInfo['scheme'], 'tcp');
+        $this->path = $this->fileInfo['path'];
 
-        $this->fileInfo = $this->GetFileData($url);
-        $this->streamsNum = is_integer($streamsNum) && $streamsNum > 1 && $this->fileInfo['acceptRanges'] ? $streamsNum : 1;
-        $this->bar = $this->fileInfo['length'] ? new ProgressBar($this->fileInfo['length']) : null;
+        $this->streamsNum = $streamsNum > 1 && $this->fileInfo['acceptRanges'] ? $streamsNum : 1;
+        if ( $this->streamsNum != $streamsNum )
+            Notifier::ShowWarning("Unable to download the file in {$streamsNum} streams. Trying to download in 1 stream");
     }
 
+    /**
+     * Downloading file
+     * @return void
+     */
     public function DownloadFile()
     {
-        $streams_r = $streams = $streams_data = [];
+        $streams_r = $streams = $streams_data = $not_finished_streams = [];
         $pieces = $this->CountDownloadPiecesNum();
+        $pieces['cnt'] = 1;
+        $data_atmts_cnt = 0;
+        $hasError = false;
 
-        $dwld_file = "{$this->downloadPath}.download.file.{$this->fileInfo['type']}";
+        $dwld_file = !empty($this->fileInfo['filename'])
+            ? "{$this->downloadPath}.download.{$this->fileInfo['filename']}"
+            : "{$this->downloadPath}.download.file.{$this->fileInfo['type']}";
 
         $this->Log('*** fileInfo ***', 'download');
         $this->Log($this->fileInfo, 'download');
@@ -62,13 +95,18 @@ class Downloader
         if ( ($file = fopen($dwld_file, 'w')) === false )
             throw new RuntimeException("Error to open the file {$dwld_file}");
 
-        $this->bar->interupt('Creating streams');
+        Notifier::ShowNotice('Download start');
+
+        if ( !$this->fileInfo['length'] )
+            $bar = new ProgressBar(1, $format = "Downloaded :current MB - Elapsed::elapseds - ETA::etas - Rate::rate/s");
+        else
+            $bar = new ProgressBar($this->fileInfo['length']);
+
+        if ( $this->streamsNum > 1 ) $bar->interupt('Creating streams');
 
         $downloaded = 0;
         while ( true )
         {
-            $num = count($streams);
-
             if ( $streams_data )
             {
                 $min = min(array_keys($streams_data));
@@ -81,11 +119,23 @@ class Downloader
                     }
             }
 
-            if ( $num < $this->streamsNum && count($streams_r) < $this->streamsNum && $pieces['pieces'] )
+            $num = count($streams);
+
+            if ( $not_finished_streams )
             {
-                $new_stream = $this->CreateStream();
-                $id = $num+1;
-                $streams[$id] = $new_stream;
+                foreach ( $not_finished_streams as $sid => $data )
+                    if ( !isset($streams_data[$sid]) )
+                    {
+                        $streams[$sid] = $this->CreateStream();
+                        $streams_data[$sid] = $data;
+                        unset($not_finished_streams[$sid]);
+                    }
+            }
+            // Calculating the download part per stream
+            elseif ( $num < $this->streamsNum && count($streams_r) < $this->streamsNum && $pieces['pieces'] )
+            {
+                $id = $pieces['cnt'];
+                $streams[$id] = $this->CreateStream();
 
                 $streams_data[$id]['skipped_headers'] = false;
                 $streams_data[$id]['length'] = 0;
@@ -106,6 +156,7 @@ class Downloader
                 $this->Log($streams_data, 'download');
 
                 $pieces['pieces']--;
+                $pieces['cnt']++;
                 continue;
             }
             elseif ( !$num && !count($streams_r) && !count($streams_data) )
@@ -120,7 +171,26 @@ class Downloader
 
             if ( !$st_read && !$st_write ) continue;
 
-            stream_select($st_read, $st_write, $e, 2, 1);
+            $str_status = stream_select($st_read, $st_write, $e, 2, 1);
+            if ( $str_status === false )
+            {
+                $bar->end();
+                throw new RuntimeException("Error in downloading the file");
+            }
+            if ( $str_status == 0 )
+            {
+                // perpetual cycle loop insurance
+                if ( self::WAITING_FOR_DATA_ATTEMTS == $data_atmts_cnt )
+                {
+                    $bar->end();
+                    Notifier::ShowWarning('Empty server answer. Interrupting the process');
+                    $hasError = true;
+                    break;
+                }
+
+                $data_atmts_cnt++;
+                continue;
+            }
 
             foreach ( $st_write as $sid => $w )
             {
@@ -178,30 +248,34 @@ class Downloader
                 }
                 else $streams_data[$sid]['data'] .= $row;
 
-                // var_dump("{$sid}|{$streams_data[$sid]['qty']}|{$streams_data[$sid]['length']}");
-                if ( $this->bar ) $this->bar->update($downloaded);
+                $amount = !$this->fileInfo['length'] ? round($downloaded/1000000, 2) : $downloaded;
+                $bar->update($amount);
 
                 if ( feof($r) || $streams_data[$sid]['qty'] == $streams_data[$sid]['length'] )
                 {
                     fclose($r);
 
-                    // var_dump('FEOF');
-                    // $this->bar->interupt("Downloaded {$downloaded}");
                     $this->Log("*** Data ***", 'download');
                     $this->Log("{$sid} - {$streams_data[$sid]['length']}", 'download');
                     $this->Log($downloaded);
 
-                    // Eсли поток не скачал указанное к-во байт
-                    // if ( $streams_data[$sid]['qty'] > $streams_data[$sid]['length'] )
-                    // {
-                    //     // открыть новый поток и докачать нужное к-во байт
-                    //     $streams[$sid] = $this->CreateStream();
-                    //     $streams_data[$sid]['skipped_headers'] = false;
+                    // If the stream did not download the specified number of bytes
+                    if ( $this->streamsNum > 1 && $streams_data[$sid]['qty'] > $streams_data[$sid]['length'] )
+                    {
+                        // open a new stream and download the desired number of bytes
+                        $not_finished_streams[$sid] = $streams_data[$sid];
 
-                    //     $streams_data[$sid]['from'] = $streams_data[$sid]['from'] + $streams_data[$sid]['length'];
-                    //     $streams_data[$id]['qty'] = $streams_data[$sid]['to'] - $streams_data[$id]['from'];
-                    //     $streams_data[$sid] = ['length' => 0, 'data' => ''];
-                    // }
+                        $not_finished_streams[$sid]['skipped_headers'] = false;
+                        $not_finished_streams[$sid]['length'] = 0;
+                        $not_finished_streams[$sid]['data'] = '';
+                        $not_finished_streams[$sid]['isDone'] = false;
+
+                        $not_finished_streams[$sid]['from'] = $not_finished_streams[$sid]['from'] + $not_finished_streams[$sid]['length'] + 1;
+                        $not_finished_streams[$sid]['qty'] = $not_finished_streams[$sid]['to'] - $not_finished_streams[$id]['from'];
+
+                        if ( $not_finished_streams[$sid]['to'] != $this->fileInfo['length'] )
+                            $not_finished_streams[$sid]['qty']++;
+                    }
 
                     $streams_data[$sid]['isDone'] = true;
                     if ( $streams_data[$sid]['data'] == '' ) unset($streams_data[$sid]);
@@ -210,10 +284,20 @@ class Downloader
             }
         }
 
+        unset($bar);
         $file = str_replace('.download.', '', $dwld_file);
-        rename($dwld_file, $file);
 
-        echo "\nThe file downloading is finished.\nThe file path: {$file}\n";
+        $i = 1;
+        while ( true )
+        {
+            if ( !file_exists($file) ) break;
+            $file = "{$i}_{$file}";
+            $i++;
+        }
+
+        rename($dwld_file, $file);
+        if ( !$hasError ) Notifier::ShowSuccess("\nThe file downloading is finished.\nThe file path: {$file}");
+        else Notifier::ShowNotice("\nThe file path: {$file}");
     }
 
     private function CreateStream()
@@ -221,7 +305,17 @@ class Downloader
         $i = 0;
         while ( $i < self::CONN_ATTEMTS_NUM )
         {
-            if ( !$fp = stream_socket_client("tcp://{$this->ip}:{$this->port}", $err_num, $err_msg, 30) )
+            if ( $this->port == 443 )
+            {
+                $opts = ['ssl' => ['SNI_enabled' => true, 'SNI_server_name' => $this->host]];
+                $context = stream_context_create($opts);
+
+                $fp = stream_socket_client("ssl://{$this->host}:443", $err_num, $err_msg, 30, STREAM_CLIENT_ASYNC_CONNECT|STREAM_CLIENT_CONNECT, $context);
+            }
+            else
+                $fp = stream_socket_client("tcp://{$this->ip}:{$this->port}", $err_num, $err_msg, 30);
+
+            if ( !$fp )
             {
                 $i++;
                 continue;
@@ -238,18 +332,65 @@ class Downloader
         throw new RuntimeException("Error to create stream: {$err_msg}", $err_num);
     }
 
-    private function GetFileData($url)
+    /**
+     * Getting file info by url
+     *
+     * @param $url - file url
+     * @return array
+     */
+    private function GetUrlInfo($url)
     {
-        if ( !$headers = get_headers($url, true) ) throw new RuntimeException("Error to get headers by the url: {$headers}");
-        if ( empty($headers['Content-Type']) ) throw new RuntimeException('Error to identify the file type');
+        if ( !($parsed_url = parse_url($url)) || empty($parsed_url['host']) || empty($parsed_url['scheme']) || empty($parsed_url['path']) )
+            throw new RuntimeException("Wrong url passed");
 
-        $file_length = !empty($headers['Content-Length']) ? intval($headers['Content-Length']) : 0;
-
-        return [
-            'type' => $this->GetFileExpByMime($headers['Content-Type']),
-            'length' => $file_length,
-            'acceptRanges' => !empty($headers['Accept-Ranges']) && $headers['Accept-Ranges'] != 'none' && $file_length > 0 ? true : false
+        $data = [
+            'host' => $parsed_url['host'],
+            'scheme' => $parsed_url['scheme'],
+            'path' => $parsed_url['path']
         ];
+
+        Notifier::ShowNotice('Getting information by url');
+
+        if ( !$headers = get_headers($url, true) ) throw new RuntimeException("Error to get headers by the url: {$url}");
+
+        if ( !empty($headers['Location']) )
+        {
+            Notifier::ShowNotice('Getting information by redirect url');
+            $url = $headers['Location'];
+
+            if ( !$headers = get_headers($url, true) )
+                throw new RuntimeException("Error to get headers by the redirect url: {$url}");
+
+            $parsed_url = parse_url($url);
+            $data['host'] = $parsed_url['host'];
+            $data['scheme'] = $parsed_url['scheme'];
+            $data['path'] = $parsed_url['path'];
+        }
+
+        if ( !empty($headers['Content-Disposition']) && $headers['Content-Disposition'] = explode(';', $headers['Content-Disposition']) )
+        {
+            foreach ( $headers['Content-Disposition'] as $c )
+                if ( strpos($c, 'filename=') !== false )
+                {
+                    $data['filename'] = str_replace('filename=', '', $c);
+                    $data['filename'] = trim($data['filename'], ' "');
+                    break;
+                }
+        }
+        elseif ( !empty($headers['Content-Type']) && $headers['Content-Type'] = explode(';', $headers['Content-Type']) )
+        {
+            $headers['Content-Type'] = trim($headers['Content-Type'][0]);
+            $data['type'] = $this->GetFileExpByMime($headers['Content-Type']);
+        }
+        else
+            throw new RuntimeException('Error to identify the file type');
+
+        $data['length'] = !empty($headers['Content-Length']) ? intval($headers['Content-Length']) : 0;
+        $data['acceptRanges'] = !empty($headers['Accept-Ranges']) && $headers['Accept-Ranges'] != 'none' && $data['length'] > 0 ? true : false;
+
+        if ( !$data['length'] ) Notifier::ShowWarning("Can't get the file length");
+
+        return $data;
     }
 
     private function GetFileExpByMime($mime)
@@ -460,7 +601,7 @@ class Downloader
 
     public function GetMemoryLimit()
     {
-        $reserve = 20971520;
+        $reserve = 10485760;
 
         $limit = ini_get('memory_limit');
         if ( $limit == -1 ) return $limit;
--- download.php
+++ download.php
@@ -1,15 +1,45 @@
 <?php
 
+// ini_set('memory_limit', '128M'); // для теста, удалить
+
 define('PATH_ROOT', dirname(__FILE__).'/');
+define('DEFAULT_STREAMS_NUM', 4);
 
 spl_autoload_register(function ($class) {
     $file = PATH_ROOT."classes/{$class}.php";
     if ( file_exists($file) ) require_once $file;
 });
 
+$uID = array_search('-u', $argv);
+if ( !$uID || empty($argv[$uID + 1]) || !$url = trim($argv[$uID + 1]) )
+{
+    Notifier::ShowError('The param -u is empty');
+    exit;
+}
+
+$s_num = DEFAULT_STREAMS_NUM;
+
+if ( $sID = array_search('-s', $argv) )
+{
+    $sID++;
+    if ( !empty($argv[$sID]) && !is_numeric($argv[$sID]) )
+    {
+        Notifier::ShowError('The param -s must be numeric');
+        exit;
+    }
+
+    $s_num = !empty($argv[$sID]) ? intval($argv[$sID]) : $s_num;
+}
+
+// http://ukrposhta.ua/postindex/upload/postvpz.zip
+
+// https://git.kernel.org/torvalds/t/linux-5.3-rc6.tar.gz
+
+// http://vs1.cdnlast.com/s/b241aebc8976d5b86397583771bb5ed5627882/hd_01/Dark.Phoenix.2019.BDRip.1080p.ukr_480.mp4
+
 try
 {
-    $dwnld = new Downloader('http://ukrposhta.ua/postindex/upload/postvpz.zip', 4);
+    $dwnld = new Downloader($url, $s_num);
     $dwnld->DownloadFile();
 }
 catch ( RuntimeException $e )
